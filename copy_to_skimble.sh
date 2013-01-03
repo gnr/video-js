31b0b5b4a54e0578463c3284526f381ca48dc32b
Gabriel Vanrenen <gabe@skimble.com>
2013-01-03 11:46:11 -0800

    script to copy resource over to code base

---
+++ copy_to_skimble.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/dev/video-js
+cp ./dist/video-js/video.min.js ~/myTickList/public/webvideoplayer/
+cp ./dist/video-js/video.js ~/myTickList/public/webvideoplayer/
+
+cp ./dist/video-js/video-js.min.css ~/myTickList/public/webvideoplayer/
+cp ./dist/video-js/video-js.png  ~/myTickList/public/webvideoplayer/
+cp ./dist/video-js/video-js.swf  ~/myTickList/public/webvideoplayer/
+
